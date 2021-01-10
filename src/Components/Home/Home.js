368149d96274a50241b19bd4bdc87893bd687a76
Giuli <giuliana@tiendanube.com>
2021-01-10 01:58:28 -0300

    adding a funny banner

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -2,11 +2,13 @@ import React from 'react'
 import './Home.css'
 import ItemListContainer from '../../Containers/ItemListContainer';
 import Item from '../Item/Item';
+import Slider from '../Slider/Slider';
 
 export default function Home() {
 
     return (
         <div className="container">
+            <Slider/>
             <div className="title">
                 <h1>Productos</h1>
             </div>
